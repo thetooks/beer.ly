826f25c5c100ffe638cc504d53e83617b2ec3c14
Benson Ninh <bensonninh@gmail.com>
2017-02-08 15:44:49 -0800

    change keys to take the values in the process environment

--- server/middleware/ssl.js
+++ server/middleware/ssl.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 // SSL Certificates for HTTPS
+
+// key: fs.readFileSync(path.join(__dirname, '../..', 'key.pem')),
+// cert: fs.readFileSync(path.join(__dirname, '../..', 'cert.pem')),
+
 module.exports = {
-  key: fs.readFileSync(path.join(__dirname, '../..', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '../..', 'cert.pem')),
+  key: process.env.KEY_PEM,
+  cert: process.env.CERT_PEM,
   passphrase: 'beerly'
 };
